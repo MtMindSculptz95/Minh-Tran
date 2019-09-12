e7a0c086edf097e4bb9a829258e0b42ad0d58f26
Minh Tran <mtran362@student.cccd.edu>
2019-09-12 05:49:02 +0000

    auto commit 09/12/19 05:49:02

--- h05/h05.cpp
+++ h05/h05.cpp
@@ -1,14 +1,25 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Minh Tran
+ *  @date Fall 2019 TTEVE
  *  @file h05.cpp
  */
 #include <string>
 #include <cmath>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 #include "h05.h"
 
 // Place the implementation (definition) of your function here
+double focalLength(double d, double r1, double r2, double n)
+{
+    double result;
+    double x = (1 / r1) - (1 / r2);
+    double y = (n - 1) * d;
+    double z = y / (n * r1 * r2);
+
+    result = (n-1) * (x + z);
+
+    return result;
+}
