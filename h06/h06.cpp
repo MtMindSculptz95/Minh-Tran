c9063af916507244a566b8ce2677f2afb2392fc7
Minh Tran <mtran362@student.cccd.edu>
2019-09-16 08:30:20 +0000

    auto commit 09/16/19 08:30:20

--- h06/h06.cpp
+++ h06/h06.cpp
@@ -1,20 +1,60 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Minh Tran
+ *  @date Fall 2019 TTEVE
  *  @file h06.cpp
  */
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?";  // Add your Canvas login name
+string STUDENT = "mtran362";  // Add your Canvas login name
 
 // Implement your function here
+string toFrenchGender(const string& country)
+{
+    string result = "who knows?";
+    size_t len = country.size();
+    string last = country.substr(len - 2,2);
+    string prefix;
+    string masculine = "Belize, Cambodge, Honduras, Mexique, Mozambique, Costa Rice, Zimbabwe";
+    string plain = "Israel, Madagascar, Sri Lanka, Singapore, Monaco, Cuba and Cyprus";
+    string backExem = "es, is, os, as";
+    string vowels = "UEOAI";
+    string islands = "iles";
 
+    auto notFound = string::npos;
+
+    if (masculine.find(country) != notFound)
+    {
+        prefix = "le ";
+    }
+    else if (plain.find(country) != notFound)
+    {
+        prefix = "";
+    }
+    else if (backExem.find(last) != notFound || country.substr(0, 4) == islands)
+    {
+        prefix = "les ";
+    }
+    else if (vowels.find(country.front()) != notFound)
+    {
+        prefix = "l'";
+    }
+    else if (country.back() == 'e' || country.back() == 'o')
+    {
+        prefix = "la ";
+    }
+    else
+    {
+        prefix = "le ";
+    }
+
+    result = prefix + country;
+    return result;
+}
 
 
 /////////////// Optional Student Code /////////////////
 int run()
 {
-    cout << "Student testing" << endl;
     return 0;
 }
