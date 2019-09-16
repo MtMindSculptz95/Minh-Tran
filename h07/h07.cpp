734ad90ffcc98af38851d742ac6de9b4897c47bc
Minh Tran <mtran362@student.cccd.edu>
2019-09-16 08:39:39 +0000

    auto commit 09/16/19 08:39:39

--- h07/h07.cpp
+++ h07/h07.cpp
@@ -1,16 +1,37 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Minh Tran
+ *  @date Fall 2019 TTEVE
  *  @file h07.cpp
  */
 #include <string>
 #include <cctype>
 using namespace std;
 
-string STUDENT = "WHO AM I?";  // Add your Canvas login name
+string STUDENT = "mtran362";  // Add your Canvas login name
 
 // Implement your function here
+int sumNums (const string& str)
+{
+    int num = 0;
+    int sum = 0;
 
+    for (size_t i = 0, len = str.size(); i < len; i++)
+    {
+        if (isdigit(str.at(i)))
+        {
+            int d = str.at(i) - '0';
+            num *= 10;
+            num += d;
+        }
+        else
+        {
+            sum += num;
+            num = 0;
+        }
+    }
+    sum += num;
+    return sum;
+}
 
 
 /////////////// Optional Student Code /////////////////
