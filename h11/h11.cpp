c930020fd4fb2e81f3b6777ccd010f105fc5784a
Minh Tran <mtran362@student.cccd.edu>
2019-10-01 22:33:14 +0000

    auto commit 10/01/19 22:33:14

--- h11/h11.cpp
+++ h11/h11.cpp
@@ -1,17 +1,67 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Minh Tran
+ *  @date TT EVE
  *  @file h11.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 // Write your function here
 void strip()
 {
+    bool inSingleCmt = false;
+    bool inMultiCmt = false;
+    bool inString = false;
+    char ch;
+
+    while(cin.get(ch))
+    {
+        if(inSingleCmt && ch == '\n')
+        {
+            inSingleCmt = false;
+        }
+        else if(inString && ch == '\\' && cin.peek() == '"')
+        {
+            inString = false;
+        }
+        else if(inMultiCmt && ch == '*' && cin.peek() == '/')
+        {
+            cin.get(ch);
+            cin.get(ch);
+            inMultiCmt = false;
+        }
+        else if(inString && ch == '"')
+        {
+            inString = false;
+        }
+        else if(!inSingleCmt && !inMultiCmt && !inString)
+        {
+            if(ch == '"')
+            {
+                inString = true;
+            }
+            else if(ch == '/' && cin.peek() == '*')
+            {
+                inMultiCmt = true;
+            }
+            else if(ch == '/' && cin.peek() == '/')
+            {
+                inSingleCmt = true;
+            }
+            else if(inString && ch == '\\' && cin.peek() == '"')
+            {
+                cout.put(ch);
+                cin.get(ch);
+            }
+        }
+        if(cin && !inSingleCmt && !inMultiCmt)
+        {
+            cout.put(ch);
+        }
+    }
 
 }
 
