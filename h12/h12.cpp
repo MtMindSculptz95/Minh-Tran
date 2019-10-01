22ab728b90c729394b566c23c4e48e88c6275afd
Minh Tran <mtran362@student.cccd.edu>
2019-10-01 22:18:00 +0000

    auto commit 10/01/19 22:17:59

--- h12/h12.cpp
+++ h12/h12.cpp
@@ -13,7 +13,42 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 // Write your function here
 void expense()
 {
-    
+    char ch;
+    while(cin.peek() != EOF)
+    {
+        while(cin.get(ch))
+        {
+            if(ch == ' ' && cin.peek() == ' ')
+            {
+                continue;
+            }
+            else if(isdigit(cin.peek()))
+            {
+                cin.unget();
+                cin.unget();
+                cin.unget();
+                break;
+            }
+            cout.put(ch);
+        }
+        cout << ", ";
+        double total = 0.0;
+        double num = 0.0;
+        while(cin.get(ch))
+        {
+            if(isdigit(cin.peek()))
+            {
+                cin.unget();
+                cin >> num;
+                total += num;
+            }
+            if(ch == '\n')
+            {
+                break;
+            }
+        }
+        cout << fixed << setprecision(2) << total << "\n";
+    }
 }
 
 //////////////// STUDENT TESTING ////////////////////
