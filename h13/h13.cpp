43fbf616c4828f0d535e87b3d653afd79415d17f
Minh Tran <mtran362@student.cccd.edu>
2019-10-03 05:57:47 +0000

    auto commit 10/03/19 05:57:46

--- h13/h13.cpp
+++ h13/h13.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author minh tran
+ *  @date TT EVE
  *  @file h08.cpp
  */
 #include <string>
@@ -10,12 +10,40 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 #include "h13.h"
 
 // Define your function here
+string dataSets(const string& fileName)
+{
+    ostringstream result;
+    ifstream in(fileName);
+    if(in.fail()) result << "data/notfound.txt cannot be opened.";
 
+    int n;
+    int data_set = 1;
+
+    while(in >> n)
+    {
+        double sum = 0.0;
+        int count = 0;
+        while(n != 0)
+        {
+            count += n;
+            double value;
+            in >> value;
+            sum += n * value;
+            in >> n;
+        }
+
+        if(data_set > 1) result << "\n";
+        result << fixed << setprecision(4);
+        result << "data set " << data_set << ": total values = " << count << "\naverage value = " << sum / static_cast<double>(count) << endl;
+        data_set++;
+    }
+    return result.str();
+}
 
 ///////////////// Student Testing /////////////////////////
 int run()
