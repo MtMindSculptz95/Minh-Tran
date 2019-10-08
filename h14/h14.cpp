d94fb60b0aa53d6c8cdaafda1c0d05e953279cd5
Minh Tran <mtran362@student.cccd.edu>
2019-10-08 23:49:41 +0000

    auto commit 10/08/19 23:49:40

--- h14/h14.cpp
+++ h14/h14.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author minh tran
+ *  @date TT EVE
  *  @file h14.cpp
  */
 #include <string>
@@ -9,11 +9,126 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 #include "h14.h"
 
 // Define your functions here
+string getLine (const string& prompt)
+{
+    string result;
+    getline(cin,result);
+
+    if(prompt.empty())
+    {
+        cout << prompt;
+    }
+    else if (! isspace(prompt.back()))
+    {
+        cout << prompt + ' ';
+    }
+    else if( isspace(prompt.back()))
+    {
+        cout << prompt;
+    }
+    return result;
+}
+
+int getInt(const string& prompt)
+{
+    int result;
+    string line = getLine(prompt);
+
+    while(true)
+    {
+        if(! line.empty())
+        {
+            istringstream in(line);
+            int n;
+            in >> n;
+            if( in.eof()&& !in.fail())
+            {
+                return n;
+            }
+            in >> ws;
+            if(in.eof() && !in.fail())
+            {
+                return n;
+            }
+        }
+        cerr << line;
+        line.clear();
+        line = getLine(prompt);
+    }
+    return result;
+
+}
+
+double getReal (const string& prompt)
+{
+    double result;
+    string line = getLine(prompt);
+
+    while(true)
+    {
+        if(! line.empty())
+        {
+            istringstream in(line);
+            double n;
+            in >> n;
+
+            if( in.eof()&& !in.fail())
+            {
+                return n;
+            }
+            in >> ws;
+
+            if(in.eof() && !in.fail())
+            {
+                return n;
+            }
+        }
+        cerr << line;
+        line.clear();
+        line = getLine(prompt);
+    }
+    return result;
+}
+bool getYN (const string& prompt)
+{
+    string line = getLine(prompt);
+
+    while (true)
+    {
+        if(! line.empty())
+        {
+        istringstream in(line);
+        string st;
+        in >> st;
+        in >> ws;
+        char ch = st.at(0);
+
+        if ((ch == 'Y' || ch == 'y' || ch == 'N' || ch == 'n'))
+            {
+                cerr << st;
+                st.clear();
+                in >> st;
+            }
+
+            if (ch == 'Y' || ch == 'y')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        cerr << "ERROR: Failed!" << endl;
+        line.clear();
+        line = getLine(prompt);
+    }
+}
 
 
 
