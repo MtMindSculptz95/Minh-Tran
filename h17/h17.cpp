5a80f1acd28bace9112fca5ef0ec046e3b20c5c7
Minh Tran <mtran362@student.cccd.edu>
2019-10-15 04:39:54 +0000

    auto commit 10/15/19 04:39:53

--- h17/h17.cpp
+++ h17/h17.cpp
@@ -12,7 +12,63 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 #include "h17.h"
 
 // Write your functions here
+/**
+ * Convert a Suit into the form "Clubs", "Hearts", etc.
+ * @param s a Suit parameter
+ * @return the string representation as described.
+ */
+std::string toString(Suit s)
+{
+    switch(s)
+    {
+        case Suit::CLUBS: return "Clubs";
+        case Suit::HEARTS: return "Hearts";
+        case Suit::SPADES: return "Spades";
+        case Suit::DIAMONDS: return "Diamonds";
+        default: return "???";
 
+    }
+}
+
+/**
+ * Convert a Rank into the form "Ace", "2", "3", "Queen", etc.
+ * @param r a Rank parameter
+ * @return the string representation as described.
+ */
+std::string toString(Rank r)
+{
+    switch(r)
+    {
+        case Rank::ACE: return "Ace";
+        case Rank::TWO: return "2";
+        case Rank::THREE: return "3";
+        case Rank::FOUR: return "4";
+        case Rank::FIVE: return "5";
+        case Rank::SIX: return "6";
+        case Rank::SEVEN: return "7";
+        case Rank::EIGHT: return "8";
+        case Rank::NINE: return "9";
+        case Rank::TEN: return "10";
+        case Rank::JACK: return "Jack";
+        case Rank::QUEEN: return "Queen";
+        case Rank::KING: return "King";
+        default: return "???";
+
+    }
+}
+
+
+/**
+ * Prints a card in the form "Ace of Spades".
+ * @param out the stream to print on.
+ * @param c the card to print.
+ * @return the stream after printing.
+ */
+std::ostream& operator<<(std::ostream& out, const Card& c)
+{
+    out << toString(c.rank) << " of " << toString(c.suit);
+    return out;
+}
 
 
 /////////////// STUDENT TESTING ////////////////////
