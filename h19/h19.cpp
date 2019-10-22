cc1b7c5a9daa8ae328051442369c5ea39d861899
Minh Tran <mtran362@student.cccd.edu>
2019-10-22 05:47:22 +0000

    auto commit 10/22/19 05:47:22

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -17,7 +17,82 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 #include "h19.h"
 
 // Place your function here
+vector<WORD> spellCheck(istream& in, const vector<string>& dictionary, const vector<string>& excluded)
+{
+    vector<WORD> results;
+    string word;
 
+    long long position;
+    while(in)
+    {
+        bool found = false;
+        string tem;
+        position = static_cast<long long >(in.tellg());
+
+        if(position == 1)
+        {
+            break;
+        }
+        in >> word >> ws;
+        for(char c : word)
+        {
+            if(! ispunct(c))
+            {
+                tem += tolower(c);
+            }
+        }
+        word = tem;
+
+        for(WORD& x:results)
+        {
+            if(x.word == word)
+            {
+                x.positions.push_back(position);
+                found = true;
+            }
+        }
+        if(found)
+        {
+            continue;
+        }
+        else if(!found)
+        {
+            for(auto excl : excluded)
+            {
+                if(excl == word)
+                {
+                    found = true;
+                }
+            }
+        }
+        if(found)
+        {
+            continue;
+        }
+        else if(!found)
+        {
+            for(auto dict : dictionary)
+            {
+                if(dict == word)
+                {
+                    found = true;
+                }
+            }
+        }
+        if(found)
+        {
+            continue;
+        }
+        else if(!found)
+        {
+            WORD temp;
+            temp.word = word;
+            temp.positions.push_back(position);
+            results.push_back(temp);
+        }
+    }
+    return results;
+}
 
 
 
