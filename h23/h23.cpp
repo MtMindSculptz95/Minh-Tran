6a83add5d4383216d84e7084e983d18b84f1e541
Minh Tran <mtran362@student.cccd.edu>
2019-10-31 07:16:05 +0000

    auto commit 10/31/19 07:16:05

--- h23/h23.cpp
+++ h23/h23.cpp
@@ -7,12 +7,51 @@
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 #include "h23.h"
 
 // Add your code here
+MinMax minMax(const double *ptr, size_t size)
+{
+    struct MinMax result;
+    result.min = ptr;
+    result.max = ptr;
 
+    auto atEnd = ptr + size;
+
+    while(ptr < atEnd)
+    {
+        ptr++;
+
+        if(ptr > result.max)
+        {
+            result.max = ptr;
+        }
+        if(ptr < result.min)
+        {
+            result.min = ptr;
+        }
+    }
+    return result;
+}
+int alternatingSum(const int a[], size_t size)
+{
+    int sum = 0;
+    int iArray[5];
+    for(size_t i = 0; i < size; i++)
+    {
+        if(a[i] % 2 == 0)
+        {
+            sum += a[i];
+        }
+        else if(a[i] % 2 != 0)
+        {
+            sum += a[i];
+        }
+    }
+    return sum;
+}
 
 /////////////// STUDENT TESTING ////////////////////
 
