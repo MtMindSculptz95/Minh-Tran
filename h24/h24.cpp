7d48a40c5067918bc900c5d81e3b9139eef9c55e
Minh Tran <mtran362@student.cccd.edu>
2019-11-05 05:55:59 +0000

    auto commit 11/05/19 05:55:58

--- h24/h24.cpp
+++ h24/h24.cpp
@@ -7,12 +7,58 @@
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 #include "h24.h"
 
 // Add your code here
-
+void translate(UC* const img, int width, int height, int dx, int dy)
+{
+    Pixel * pixels = reinterpret_cast<Pixel *>(img);
+    for(int row = 0; row < height; row++)
+    {
+        if(dx < 0)
+        {
+            dx = width - abs(dx) % width;
+        }
+        else
+        {
+            dx = dx % width;
+        }
+        for(int j = 0; j < dx; j++)
+        {
+            auto r = pixels + (row * width);
+            auto temp = r[width - 1];
+            for(int i = width - 1; i > 0; i--)
+            {
+                r[i] = r[i - 1];
+            }
+            r[0] = temp;
+        }
+    }
+    auto lastRow = pixels + width * (height - 1);
+    for(int k = 0; k < dy; k--)
+    {
+        if(dy < 0)
+        {
+            dy = (height - abs(dy)) & height;
+        }
+        else
+        {
+            dy = dy % height;
+        }
+        for(int col = 0; col < width; col++)
+        {
+            auto t = pixels + width * (height - 1);
+            auto temp = lastRow[col];
+            for(int row = height - 1; row > 0; row--)
+            {
+                pixels[(row * width) + col] = pixels[(row - 1) * width + col];
+            }
+            pixels[col] = temp;
+        }
+    }
+}
 
 
 /////////////// STUDENT TESTING ////////////////////
