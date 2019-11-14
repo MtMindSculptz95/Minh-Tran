175a1d64c78892aed912640cc6e1a837345ba813
Minh Tran <mtran362@student.cccd.edu>
2019-11-14 04:24:51 +0000

    auto commit 11/14/19 04:24:51

--- h27/h27.cpp
+++ h27/h27.cpp
@@ -8,12 +8,53 @@
 #include <memory>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 #include "h27.h"
 
 // Add your code here
+FlexArray& readData(istream& in, FlexArray& a)
+{
+    size_t capacity = INITIAL_CAPACITY;
+    a.data_ = unique_ptr<int[]> (new int[capacity]);
+    int read = 0;
+    size_t count = 0;
+    while(in >> read && capacity > count && !in.fail())
+    {
+        a.data_[count++] = read;
+        a.size_++;
+        if(a.size_ == capacity)
+        {
+            unique_ptr<int[]> b;
+            b.reset(a.data_.release());
+            a.data_ = unique_ptr<int[]>(new int[capacity *= 2]);
+            for(size_t i = 0; i != a.size_; i++)
+            {
+                a.data_[i] = b[i];
+            }
+        }
+    }
+    unique_ptr<int[]> b;
+    b.reset(a.data_.release());
+    a.data_ = unique_ptr<int[]>(new int[a.size_]);
+    for(size_t k = 0; k != a.size_; k++)
+    a.data_[k] = b[k];
+    return a;
+}
 
+string toString(const FlexArray& a)
+{
+    string result = "{";
+    if(a.size_ > 0)
+    {
+        result += to_string(a.data_[0]);
+    }
+    for(size_t i = 1; i < a.size_; i++)
+    {
+        result += ", " + to_string(a.data_[i]);
+    }
+    return result + "}";
+}
 
 //////////////////////// STUDENT TESTING //////////////////////////
 #include <iostream>
