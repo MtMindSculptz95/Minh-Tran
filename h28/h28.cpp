7b69a6ddd06684c9c2780053ae0a189a621f32fb
Minh Tran <mtran362@student.cccd.edu>
2019-11-14 04:05:09 +0000

    auto commit 11/14/19 04:05:08

--- h28/h28.cpp
+++ h28/h28.cpp
@@ -6,15 +6,61 @@
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 #include "h28.h"
 
 // Implement the Bug class member functions here
+Bug::Bug(int startPos)
+{
+    position_ = startPos;
+    direction_ = 1;
+}
+void Bug::move()
+{
+    if(direction_ == 1) position_++;
+    else if(direction_ == 0) position_--;
+}
+void Bug::turn()
+{
+    if(direction_ == 0)
+    {
+        direction_ = 1;
+    }
+    else if(direction_ == 1)
+    {
+        direction_ = 1;
+    }
+}
+int Bug::position() const
+{
+    return position_;
+}
 
 
 // Implement the Employee class member functions here
-
+Employee::Employee()
+{
+    salary_ = 0;
+}
+Employee::Employee(const string& name, double salary)
+{
+    salary_ = salary;
+    Person data = {name, data_.age()};
+    data_ = data;
+}
+void Employee::salary(double newSalary)
+{
+    salary_ = newSalary;
+}
+double Employee::salary() const
+{
+    return salary_;
+}
+string Employee::name() const
+{
+    return data_.name();
+}
 
 
 //////////////////////// STUDENT TESTING //////////////////////////
