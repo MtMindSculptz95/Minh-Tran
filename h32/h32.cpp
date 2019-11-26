f66f5994a0247af16dfcb196cddfb1e23381fbf5
Minh Tran <mtran362@student.cccd.edu>
2019-11-26 23:59:44 +0000

    auto commit 11/26/19 23:59:44

--- h32/h32.cpp
+++ h32/h32.cpp
@@ -9,13 +9,18 @@ using namespace std;
 
 #include "h32.h"
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 // Add your implementation here
+string stringClean(const string& s)
+{
+    if (s.length() < 2) return s;
 
-
-
-
+    if (s[0] == s[1])
+    return stringClean(s.substr(1));
+    else
+    return s[0] + stringClean(s.substr(1));
+}
 ////////////////// Student Testing //////////////////////////
 #include <iostream>
 int run()
