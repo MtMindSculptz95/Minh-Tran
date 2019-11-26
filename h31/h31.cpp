6a0c67210f7e95e8abf91479eb466d8e70764afc
Minh Tran <mtran362@student.cccd.edu>
2019-11-26 23:58:57 +0000

    auto commit 11/26/19 23:58:57

--- h31/h31.cpp
+++ h31/h31.cpp
@@ -16,15 +16,18 @@ bool find(const string& s, const string& t)
 {
     size_t x = t.size();
 
-    if (s.size() < x) { return false; }
+    if (s.size() < x)
+    {
+        return false;
 
-    else if (s.substr(0, x) == t) { return true; }
+    }
+    else if (s.substr(0, x) == t)
+    {
+        return true;
 
+    }
     return find(s.substr(1) , t);
 }
-
-
-
 ////////////////// Student Testing //////////////////////////
 #include <iostream>
 int run()
