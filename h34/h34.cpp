a49b74d71a5f03675556fad72933da838117ea69
Minh Tran <mtran362@student.cccd.edu>
2019-12-04 00:51:14 +0000

    auto commit 12/04/19 00:51:14

--- h34/h34.cpp
+++ h34/h34.cpp
@@ -18,60 +18,47 @@ Circle::Circle(double r, double x, double y) : Point( x, y )
 {
     radius = r;
 }
-
 double Circle::getRadius() const
 {
     return radius;
 }
-
 double Circle::getArea() const
 {
     return PI * radius * radius;
 }
-
 double Circle::getCircumference() const
 {
     return 2 * PI * radius;
 }
-
 std::string Circle::toString(int decimals) const
 {
     ostringstream out;
     out << fixed << setprecision(decimals) << radius;
     return "Circle(radius=" + out.str() + ", center=" + Point::toString(decimals) +")";
 }
-
 Cylinder::Cylinder(double h, double r, double x, double y) : Circle(r, x, y)
 {
 
     height = h;
 }
-
 double Cylinder::getHeight() const
 {
     return height;
 }
-
 double Cylinder::getVolume() const
 {
     return Circle::getArea() * Cylinder::getHeight();
 }
-
 double Cylinder::getArea() const
 {
     return Circle::getCircumference() * Cylinder::getHeight() + 2 * Circle::getArea();
 }
-
 string Cylinder::toString(int decimals) const
 {
     ostringstream out;
     out << fixed << setprecision(decimals) << height;
     return "Cylinder(height=" + out.str() + ", base=" + Circle::toString(decimals) + ")";
-
 }
-
-
-
 //////////////// Student Tests ////////////////////////////
 #include <iostream>
 int run()
