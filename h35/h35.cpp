ace7954d0d33dd5a4cbff3e854f04b05662c9335
Minh Tran <mtran362@student.cccd.edu>
2019-12-06 01:00:36 +0000

    auto commit 12/06/19 01:00:35

--- h35/h35.cpp
+++ h35/h35.cpp
@@ -11,10 +11,44 @@ using namespace std;
 
 #include "h35.h"
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mtran362"; // Add your Canvas/occ-email ID
 
 // Add your implementation here
+Worker:: Worker(const string& NAME, double RATE)
+{
+    name = NAME;
+    rate = RATE;
+}
+string Worker::getName() const { return name; }
+double Worker::getRate() const { return rate; }
 
+SalariedWorker::SalariedWorker(const string& NAME, double RATE) : Worker(NAME, RATE) {}
+string SalariedWorker::payInfo(int hours)
+{
+    ostringstream out;
+    out << name << "(Salaried, $ " << fixed << setprecision(2) << rate << ") worked " << hours << " hours. Pay: $ " << fixed << setprecision(2) << 40 * rate;
+
+    return out.str();
+}
+
+HourlyWorker::HourlyWorker(const string& NAME, double RATE) : Worker(NAME, RATE) {}
+string HourlyWorker::payInfo(int hours)
+{
+    double pay = 0;
+    if(hours <= 40)
+    {
+        pay = hours * rate;
+    }
+    else if(hours > 40)
+    {
+        pay = 1017.50;
+    }
+
+    ostringstream out;
+    out << name << "(Hourly, $ " << fixed << setprecision(2) << rate << ") worked " << hours << " hours. Pay: $ " << fixed << setprecision(2) << pay;
+
+    return out.str();
+}
 
 
 /////////////// Student Tests ////////////////////////////
